096700a8dc41173720f1e11383e3e61fa658b4b4
jeremiahlee94 <jhwlee94@gmail.com>
2023-01-05 13:21:27 -0800

    create schema

--- models/tax.js
+++ models/tax.js
@@ -1,2 +1,20 @@
 // require mongoose package from the connection pool
 const mongoose = require('../db/connection');
+
+// schema
+const TaxSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    tax: [
+        {
+            year: Number,
+            taxesOwed: Number,
+        }
+    ],
+});
+
+// instantiate the model
+const Tax = mongoose.model('Tax', TaxSchema);
+
+// export
+module.exports = Tax;
