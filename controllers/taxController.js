914f819057374a4d2680786ce2567156e3ae079b
jeremiahlee94 <jhwlee94@gmail.com>
2023-01-05 13:34:54 -0800

    add get route

--- controllers/taxController.js
+++ controllers/taxController.js
@@ -1,8 +1,25 @@
+// require express module
+const express = require('express');
+
+// instantiate the router
+const router = express.Router();
+
+// import Tax model
 const Tax = require('../models/Tax');
 
 // ROUTES
 
 // GET, index
+router.get('/', async (req, res, next) => {
+    try {
+        // get all orders
+        const tax = await Tax.find({});
+        // send response in JSON
+        res.json(tax);
+    } catch(err) {
+        next(err);
+    }
+});
 
 // GET, single
 
